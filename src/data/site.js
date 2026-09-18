fd407f8eb7698fcd62a648b6177a4c3236275d62
SalmanAli <salmanali162009@gmail.com>
2026-09-18 07:12:54 +0500

    solve bug

--- src/data/site.js
+++ src/data/site.js
@@ -22,5 +22,8 @@ export const site = {
     'Hello Golden Trip Tours & Travels, I would like to know more about your travel packages.',
 }
 
-export const whatsappLink = (message = site.whatsappMessage) =>
-  `https://wa.me/${site.whatsapp}?text=${encodeURIComponent(message)}`
+// export const whatsappLink = (message = site.whatsappMessage) =>
+ export const whatsappLink = () =>
+
+  'https://api.whatsapp.com/send?phone=%2B923452929973&token=eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjEyNSJ9.eyJleHAiOjE3ODk3ODIxOTksInBob25lIjoiKzkyMzQ1MjkyOTk3MyIsImNvbnRleHQiOiJBZmhOcE50RlhzajB1Y00wU05qeDdzOFdjbVNvaE11OFhXNlZ4bGRlXzYwRFJNTzlzOGZXZXRIclIzZWMwN3FaWUp0VFFSZFpad1RBWjBsTmdiX0dxSXRMSjBIWkxiMFpPUlBJUzJqeG1GOHhlNmt5YkhXaERDVDdKUVQyQW96M1JrNWx2cnFwdWJMNGM2aWhtUktBNHF0amJRIiwic291cmNlIjoiRkJfUGFnZSIsImFwcCI6ImZhY2Vib29rIiwiZW50cnlfcG9pbnQiOiJwYWdlX2N0YSJ9.gLTMKHAfKicdM8uerB2GtPAgOTsUQCGE-9FQK2tvKfKB2zuMFv2eZW3Ab3S2tnG3d3bLflGMGQGHlybfJ7a6Cw&fbclid=IwcGRvZgVleHRuA2FlbQIxMABzcnRjBmFwcF9pZBAyMjIwMzkxNzg4MjAwODkyAAEeJsZ-qmKnEgEHb-EIKaOhQORMZKXWiz37NJbV-o6Lc4x8rzDqpnFHxF1XfSY_aem_SaWdNcvkF8oqCbHjvocHHw'
+  // `https://wa.me/${site.whatsapp}?text=${encodeURIComponent(message)}`
